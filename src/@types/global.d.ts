e6c3dfb594b8dd0704ff932ff7bdd0b853d69bcc
--get-all <--get-all>
2020-09-20 20:47:48 -0500

    frivelous type stuff removal

--- src/@types/global.d.ts
+++
@@ -1,3 +0,0 @@
-import * as API from '~/API';
-
-type Group = Omit<Exclude<API.GetGroupQuery['getGroup'], null>, '__typename'>;
--- src/components/types.d.ts
+++
@@ -1,3 +0,0 @@
-import * as APIt from '~/API';
-
-type Group = Omit<Exclude<APIt.GetGroupQuery['getGroup'], null>, '__typename'>;
